f558e832fc377e2df887709698d55d1c2f2f17a3
quothbonney <jackie.d.carson@gmail.com>
2022-07-22 13:25:12 -0500

    Support for other image types + other fun stuff

--- scraping/webpie.py
+++ scraping/webpie.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
 Usage: ./webpie.py <URL> <DIRECTORY>
+
+Scrapes websites for images by echoing network requests
 """
 
 from seleniumwire import webdriver  # Import from seleniumwire
 from selenium.webdriver.firefox.options import Options
 import requests
 import os, sys
+
+S_THRESHOLD = 90000
 
 print("Started application")
 
@@ -30,13 +34,20 @@ headers = {"Accept-Encoding": 'identity'}
 # Naming images
 img_num = 1
 
+
 # Access requests via the `requests` attribute
 for request in driver.requests:
+    # split header into initator and type
+    header = str(request.response.headers['Content-Type'])
+    initator, _ = header.split('/')
+
     # Filters out unwanted requests
-    if request.response and request.response.headers['Content-Type'] == 'image/webp':
+    if request.response and initator == "image":
         r = requests.get(request.url, headers=headers)
-        with open(f"{filepath}/{img_num}.jpg", 'wb') as f:
-            f.write(r.content)
+        # Check sufficient image size (avoid thumbnails)
+        if len(r.content) > S_THRESHOLD: 
+            with open(f"{filepath}/{img_num}.jpg", 'wb') as f:
+                f.write(r.content)
         
         img_num += 1
         print(f"Downloaded image from {request.url} to {filepath}\n")
